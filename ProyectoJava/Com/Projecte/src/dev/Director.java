d9dc30752e34af034b4c40c4ce6d233a4bf9d670
Josep Juan Tortosa <90142578+NECROSS20173@users.noreply.github.com>
2025-05-22 20:03:13 +0200

    Delete ProyectoJava/Com/Projecte directory

--- ProyectoJava/Com/Projecte/src/dev/Director.java
+++
@@ -1,9 +0,0 @@
-package Com.Projecte.src.dev;
-
-public class Director extends Persona {
-
-    public Director(String nombre, String apellido, String poblacio, int fechaNacimiento, int id) {
-        super(nombre, apellido, poblacio, fechaNacimiento, id);
-    }
-
-}
