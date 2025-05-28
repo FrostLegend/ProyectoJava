67c0360af1b5be5053503cb0d0e7d8762e1c1745
Josep Juan Tortosa <90142578+NECROSS20173@users.noreply.github.com>
2025-05-28 15:29:00 +0200

    en teoria esta bien xd

---
+++ Com/Projecte/Alejandro/DatosAlejandro.java
@@ -0,0 +1,8 @@
+package Com.Projecte.Alejandro;
+public class DatosAlejandro {
+    private static String nombre = "Alejandro";
+
+    public static void mostrarNombre() {
+        System.out.println("Encargado GitHub: " + nombre);
+    }
+}
