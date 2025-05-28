1423e3da7dd48a65f668120b6aa9f3bfe168a79c
Josep Juan Tortosa <90142578+NECROSS20173@users.noreply.github.com>
2025-05-28 16:45:21 +0200

    Delete Com/Projecte directory

--- Com/Projecte/Alejandro/DatosAlejandro.java
+++
@@ -1,8 +0,0 @@
-package Com.Projecte.Alejandro;
-public class DatosAlejandro {
-    private static String nombre = "Alejandro";
-
-    public static void mostrarNombre() {
-        System.out.println("Encargado GitHub: " + nombre);
-    }
-}
