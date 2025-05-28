082fdbafb182f0b87dbd46bb60708d9eee371963
Josep Juan Tortosa <90142578+NECROSS20173@users.noreply.github.com>
2025-05-28 15:38:47 +0200

    Delete Com/Projecte directory

--- Com/Projecte/Alvaro/DatosAlvaro.java
+++
@@ -1,9 +0,0 @@
-package Com.Projecte.Alvaro;
-
-public class DatosAlvaro {
-    private static String nombre = "Alvaro";
-
-    public static void mostrarNombre() {
-        System.out.println("Responsable BBDD de Java: (Caido en combate)" + nombre);
-    }
-}
--- Com/Projecte/Raul/DatosRaul.java
+++
@@ -1,7 +0,0 @@
-package Com.Projecte.Raul;
-
-public class DatosRaul {
-    public static void mostrarNombre() {
-        System.out.println("Soporte Auxiliar: Raul");
-    }
-}
